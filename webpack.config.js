10cd83d055a76b58604c5310bd74a1471dccaabc
Trotyl <trotyl@qq.com>
2016-07-08 13:02:12 +0800

    Finish THE HERO EDITOR in tutorial.

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ console.log(`Start webpack in: ${env}`)
 
 switch (env) {
     case 'local':
-        module.exports = require('./config/webpack.debug.js')
+        module.exports = require('./config/webpack.debug')
         break
     default:
-        module.exports = require('./config/webpack.deploy.js')
+        module.exports = require('./config/webpack.deploy')
 }
